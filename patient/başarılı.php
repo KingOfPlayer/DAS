3ef02016872c5e516ab912cb1f330402875de68e
ilberozgurdayi <73353700+ilberozgurdayi@users.noreply.github.com>
2024-05-03 22:27:53 +0300

    çalışan sorgu

--- patient/başarılı.php
+++ patient/başarılı.php
@@ -1,14 +1,22 @@
 <?php
+// Veritabanı bağlantısı
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$dbname = "DAS";
 
-include("logincheck.php");
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-include("../database.php");
+// Bağlantı kontrolü
+if ($conn->connect_error) {
+    die("Veritabanı bağlantısı başarısız: " . $conn->connect_error);
+}
 
 // Şehirleri veritabanından al
 $sql = "SELECT * FROM city";
 $sql1= "select * from doctor_specialty";
-$result = $database->query($sql);
-$result1 = $database->query($sql1)
+$result = $conn->query($sql);
+$result1 = $conn->query($sql1)
 ?>
 
 
@@ -23,20 +31,66 @@ $result1 = $database->query($sql1)
     <body>
 	
 		<!-- Preloader -->
-        <?php
-			include("../templates/preloader.php");
-		?>
+        <div class="preloader">
+            <div class="loader">
+                <div class="loader-outter"></div>
+                <div class="loader-inner"></div>
+
+                <div class="indicator"> 
+                    <svg width="16px" height="12px">
+                        <polyline id="back" points="1 6 4 6 6 11 10 1 12 6 15 6"></polyline>
+                        <polyline id="front" points="1 6 4 6 6 11 10 1 12 6 15 6"></polyline>
+                    </svg>
+                </div>
+            </div>
+        </div>
         <!-- End Preloader -->
 
-		<!-- Nav -->
-		<?php
-			include("./nav.php");
-		?>
-        <!-- End Nav -->
+		<!-- Header Area -->
+		<header class="header" >
+		
+			<!-- Header Inner -->
+			<div class="header-inner">
+				<div class="container">
+					<div class="inner">
+						<div class="row">
+							<div class="col-lg-3 col-md-3 col-12">
+								<!-- Start Logo -->
+								<div class="logo">
+									<a href="/index.html"><img src="/img/logo.png" alt="#"></a>
+								</div>
+								<!-- End Logo -->
+								<!-- Mobile Nav -->
+								<div class="mobile-nav"></div>
+								<!-- End Mobile Nav -->
+							</div>
+							<div class="col-lg-7 col-md-9 col-12">
+								<!-- Main Menu -->
+								<div class="main-menu">
+									<nav class="navigation">
+										<ul class="nav menu">
+											<li class="active"><a href="/#">anasayfa </a></li>
+											<li><a href="/doktorlar.php">doktorlar </a></li>
+											<li ><a href="/doktorlar">randevularım </a></li>
+											<li  ><div style="margin-top: 20px;">
+                                           <button class="btn btn-primary">çıkış yap</button>
+                                            </div> </li> 
+											
+										
+									</nav>
+								</div>
+								<!--/ End Main Menu -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Header Inner -->
+		</header>
 
         <!--arama alanı -->
         
-        <div class="container margin-tb-5rem">
+        <div class="container margin-5rem">
     <div class="row justify-content-center mt-5">
         <div class="col-md-6">
             <form action="submit.php" method="post">
@@ -96,4 +150,3 @@ $result1 = $database->query($sql1)
 		?>
     </body>
 </html>
-
--- patient/submit.php
+++ patient/submit.php
@@ -1,54 +1,154 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+    <head>
+		<?php
+			include("../templates/importcss.php");
+		?>
+    </head>
+    <body>
+	
+		<!-- Preloader -->
+        <div class="preloader">
+            <div class="loader">
+                <div class="loader-outter"></div>
+                <div class="loader-inner"></div>
+
+                <div class="indicator"> 
+                    <svg width="16px" height="12px">
+                        <polyline id="back" points="1 6 4 6 6 11 10 1 12 6 15 6"></polyline>
+                        <polyline id="front" points="1 6 4 6 6 11 10 1 12 6 15 6"></polyline>
+                    </svg>
+                </div>
+            </div>
+        </div>
+        <!-- End Preloader -->
+
+		<!-- Header Area -->
+		<header class="header" >
+		
+			<!-- Header Inner -->
+			<div class="header-inner">
+				<div class="container">
+					<div class="inner">
+						<div class="row">
+							<div class="col-lg-3 col-md-3 col-12">
+								<!-- Start Logo -->
+								<div class="logo">
+									<a href="/index.html"><img src="/img/logo.png" alt="#"></a>
+								</div>
+								<!-- End Logo -->
+								<!-- Mobile Nav -->
+								<div class="mobile-nav"></div>
+								<!-- End Mobile Nav -->
+							</div>
+							<div class="col-lg-7 col-md-9 col-12">
+								<!-- Main Menu -->
+								<div class="main-menu">
+									<nav class="navigation">
+										<ul class="nav menu">
+											<li class="active"><a href="/#">anasayfa </a></li>
+											<li><a href="/doktorlar.php">doktorlar </a></li>
+											<li ><a href="/doktorlar">randevularım </a></li>
+											<li  ><div style="margin-top: 20px;">
+                                           <button class="btn btn-primary">çıkış yap</button>
+                                            </div> </li> 
+											
+										
+									</nav>
+								</div>
+								<!--/ End Main Menu -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Header Inner -->
+		</header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php
 // Veritabanı bağlantısı
-$servername = "localhost";
-$username = "root";
-$password = "1234";
-$dbname = "das";
-// Veritabanı bağlantısını oluştur
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Bağlantıyı kontrol et
-if ($conn->connect_error) {
-    die("Veritabanı bağlantısı başarısız: " . $conn->connect_error);
-}
-
+include ('../database.php');
 // Formdan gelen verileri al
+
 $sehir = $_POST['sehir'];
 $doctor_specialty = $_POST['doctor_specialty'];
 
 // Doktorları veritabanından al ve uygun olanları filtrele
-$sql = "SELECT * FROM das.doctors 
-        JOIN das.city ON das.doctors.city_id = das.city.id
-        JOIN doctor_specialty ON das.doctors.doctor_specialty_id = doctor_specialty.id
-        WHERE das.city.id = $sehir and doctor_specialty.id= $doctor_specialty    ";
-$result = $conn->query($sql);
+$sql = "SELECT 
+das.doctors.*,das.city.name AS 'city_name',doctor_specialty.name AS 'doctor_specialty_name',doctor_degree.name AS doctor_degree_name
+FROM
+das.doctors
+    JOIN
+das.city ON das.doctors.city_id = city.id
+    JOIN
+doctor_specialty ON doctors.doctor_specialty_id = doctor_specialty.id 
+     join doctor_degree on doctors.doctor_degree_id=doctor_degree.id    
+        WHERE das.city.id = $sehir and doctor_specialty.id= $doctor_specialty";
+$result = $database->query($sql);
 
 // Sonucu ekrana yazdır
 if ($result->num_rows > 0) {
-    echo "<div class='container margin-5rem'>";
-    echo "<div class='row justify-content-center mt-5'>";
-    echo "<div class='col-md-6'>";
-    echo "<div class='alert alert-success' role='alert'>Uygun doktorlar bulundu:</div>";
-    echo "<div class='doktorlar'>";
+   
+   
     while($row = $result->fetch_assoc()) {
-        echo "<p>" . $row['name'] . " " . $row['surname'] . " - " . $row['doctor_specialty'] . "</p>";
+        echo "<p>" . $row['name'] . " " . $row['surname'] . " - " . $row['doctor_specialty_name'] ."-". $row['doctor_degree_name']."</p>";
+        echo "<br>.<br>";
+      // tasarım ve takvim buraya
+        
     }
-    echo "</div>"; // doktorlar divi
-    echo "</div>"; // col
-    echo "</div>"; // row
-    echo "</div>"; // container
+   
+    
 } else {
-    echo "<div class='container margin-5rem'>";
-    echo "<div class='row justify-content-center mt-5'>";
-    echo "<div class='col-md-6'>";
-    echo "<div class='alert alert-warning' role='alert'>Uygun doktor bulunamadı.</div>";
-    echo "$sehir + $doctor_specialty ";
-    echo "</div>"; // col
-    echo "</div>"; // row
-    echo "</div>"; // container
+    
+    echo "<div class='alert alert-warning'>";
+    echo "<div class='alert alert-warning' role='alert'>Uygun doktor bulunamadı.</div>"; //alert
+  
 }
-echo $sql;
+
+
 
 // Veritabanı bağlantısını kapat
-$conn->close();
+
 ?>
+
+
+</div>
+        
+        
+        
+        
+        
+        
+        </div>
+        
+    
+    </div>
+	<!--arama alanı-->
+
+		<?php
+			include("../templates/importfooter.php");
+			include("../templates/importjs.php");
+		?>
+    </body>
+</html>
