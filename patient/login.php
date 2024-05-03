05685d6856dcf5d5230e722746a6a53c91e1f34a
Erkan Hazır <41erkan2002@gmail.com>
2024-05-03 10:23:29 +0300

    Templates
    Sayfaların değişmeyen bazı yerlerini modüler hale getirdim

--- patient/login.php
+++ patient/login.php
@@ -42,47 +42,9 @@ if(isset($_GET['action'])){
 <!doctype html>
 <html class="no-js" lang="zxx">
     <head>
-        <!-- Meta Tags -->
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="keywords" content="Site keywords here">
-		<meta name="description" content="">
-		<meta name='copyright' content=''>
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		
-		<!-- Title -->
-        <title>medikal servis </title>
-		
-		<!-- Favicon -->
-        <link rel="icon" href="/img/favicon.png">
-		
-		<!-- Google Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
-
-		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="/css/bootstrap.min.css">
-		<!-- Nice Select CSS -->
-		<link rel="stylesheet" href="/css/nice-select.css">
-		<!-- Font Awesome CSS -->
-        <link rel="stylesheet" href="/css/font-awesome.min.css">
-		<!-- icofont CSS -->
-        <link rel="stylesheet" href="/css/icofont.css">
-		<!-- Slicknav -->
-		<link rel="stylesheet" href="/css/slicknav.min.css">
-		<!-- Owl Carousel CSS -->
-        <link rel="stylesheet" href="/css/owl-carousel.css">
-		<!-- Datepicker CSS -->
-		<link rel="stylesheet" href="/css/datepicker.css">
-		<!-- Animate CSS -->
-        <link rel="stylesheet" href="/css/animate.min.css">
-		<!-- Magnific Popup CSS -->
-        <link rel="stylesheet" href="/css/magnific-popup.css">
-		
-		<!-- Medipro CSS -->
-        <link rel="stylesheet" href="/css/normalize.css">
-        <link rel="stylesheet" href="/style.css">
-        <link rel="stylesheet" href="/css/responsive.css">
-		
+		<?php
+			include("../templates/importcss.php");
+		?>
     </head>
     <body>
 	
@@ -101,10 +63,7 @@ if(isset($_GET['action'])){
             </div>
         </div>
         <!-- End Preloader -->
-		
-		<!-- Get Pro Button -->
-		
-	
+
 		<!-- Header Area -->
 		<header class="header" >
 			<!-- Topbar -->
@@ -190,179 +149,55 @@ if(isset($_GET['action'])){
 
         <!--Login Screen -->
         <div class="container">
-    <div class="row justify-content-center mt-5">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header text-center">Giriş Yap</div>
-                <div class="card-body">
-                    <form action="login.php?action=login" method="POST">
-                        <div class="form-group">
-                            <label for="email">E-posta Adresi:</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Şifre:</label>
-                            <input type="password" class="form-control" id="password" name="password" required>
-                        </div>
+			<div class="row justify-content-center mt-5">
+				<div class="col-md-6">
+					<div class="card">
+						<div class="card-header text-center">Giriş Yap</div>
+						<div class="card-body">
+							<form action="login.php?action=login" method="POST">
+								<div class="form-group">
+									<label for="email">E-posta Adresi:</label>
+									<input type="email" class="form-control" id="email" name="email" required>
+								</div>
+								<div class="form-group">
+									<label for="password">Şifre:</label>
+									<input type="password" class="form-control" id="password" name="password" required>
+								</div>
 
-						<?php
-						//Mesaj bölümü 
-							if (isset($msg)) {
-								echo '<div class="alert ';
-								echo $msg["type"];
-								echo '" role="alert">';
-								echo $msg["text"];
-								echo '</div>';
+								<?php
+								//Mesaj bölümü 
+									if (isset($msg)) {
+										echo '<div class="alert ';
+										echo $msg["type"];
+										echo '" role="alert">';
+										echo $msg["text"];
+										echo '</div>';
 
-								echo '<script>history.pushState({}, "", "login.php");</script>';
-							}else{
-								echo "<br>";
-							}
-						?>
+										echo '<script>history.pushState({}, "", "login.php");</script>';
+									}else{
+										echo "<br>";
+									}
+								?>
 
-						<button type="submit" class="btn btn-primary btn-block">Giriş Yap</button>
-						<br>			
-                    </form>
-					<br>
-					<form action="logout.php" method="POST">
-                         <button class="btn btn-primary btn-block" type="submit">Oturumu Kapat</button>
-                         </form>
+								<button type="submit" class="btn btn-primary btn-block">Giriş Yap</button>
+								<br>			
+							</form>
+							<br>
+							<form action="logout.php" method="POST">
+								 <button class="btn btn-primary btn-block" type="submit">Oturumu Kapat</button>
+								 </form>
                         
 
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!--login screen-->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<!--login screen-->
 
-		<!-- End Header Area -->
-		<footer id="footer" class="footer ">
-			<!-- Footer Top -->
-			<div class="footer-top">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-3 col-md-6 col-12">
-							<div class="single-footer">
-								<h2>hakkımızda</h2>
-								<p>sosyal medya hesaplarımız</p>
-								<!-- Social -->
-								<ul class="social">
-									<li><a href="/#"><i class="icofont-facebook"></i></a></li>
-									<li><a href="/#"><i class="icofont-google-plus"></i></a></li>
-									<li><a href="/#"><i class="icofont-twitter"></i></a></li>
-									<li><a href="/#"><i class="icofont-vimeo"></i></a></li>
-									<li><a href="/#"><i class="icofont-pinterest"></i></a></li>
-								</ul>
-								<!-- End Social -->
-							</div>
-						</div>
-						<div class="col-lg-3 col-md-6 col-12">
-							<div class="single-footer f-link">
-								<h2>Quick Links</h2>
-								<div class="row">
-									<div class="col-lg-6 col-md-6 col-12">
-										<ul>
-											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>anasayfa</a></li>
-											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>hakkımzda</a></li>
-											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>hizmetler</a></li>
-											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>vakalar</a></li>
-											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>diğer bağlantılar</a></li>	
-										</ul>
-									</div>
-									<div class="col-lg-6 col-md-6 col-12">
-										<ul>
-											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>danışmanlık</a></li>
-											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>finans</a></li>
-											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>Testimonials</a></li>
-											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>SSS</a></li>
-											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>bize ulaşın</a></li>	
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-3 col-md-6 col-12">
-							<div class="single-footer">
-								<h2>açık saatler</h2>
-								<p>ayarlanacak!!!!</p>
-								<ul class="time-sidual">
-									<li class="day">Monday - Fridayp <span>8.00-20.00</span></li>
-									<li class="day">Saturday <span>9.00-18.30</span></li>
-									<li class="day">Monday - Thusday <span>9.00-15.00</span></li>
-								</ul>
-							</div>
-						</div>
-						<div class="col-lg-3 col-md-6 col-12">
-							<div class="single-footer">
-								<h2>Newsletter</h2>
-								<p>en yeni haberler için kayıt olmayı unutmayın</p>
-								<form action="mail/mail.php" method="get" target="_blank" class="newsletter-inner">
-									<input name="email" placeholder="Email Address" class="common-input" onfocus="this.placeholder = ''"
-										onblur="this.placeholder = 'Your email address'" required="" type="email">
-									<button class="button"><i class="icofont icofont-paper-plane"></i></button>
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!--/ End Footer Top -->
-			<!-- Copyright -->
-			<div class="copyright">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-12 col-md-12 col-12">
-							<div class="copyright-content">
-								<p>© Copyright 2024  |  All Rights Reserved by <a href="https://www.wpthemesgrid.com" target="_blank">kocaeli üniversitesi</a> </p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!--/ End Copyright -->
-		</footer>
-		<!--/ End Footer Area -->
-		
-		<!-- jquery Min JS -->
-        <script src="/js/jquery.min.js"></script>
-		<!-- jquery Migrate JS -->
-		<script src="/js/jquery-migrate-3.0.0.js"></script>
-		<!-- jquery Ui JS -->
-		<script src="/js/jquery-ui.min.js"></script>
-		<!-- Easing JS -->
-        <script src="/js/easing.js"></script>
-		<!-- Color JS -->
-		<script src="/js/colors.js"></script>
-		<!-- Popper JS -->
-		<script src="/js/popper.min.js"></script>
-		<!-- Bootstrap Datepicker JS -->
-		<script src="/js/bootstrap-datepicker.js"></script>
-		<!-- Jquery Nav JS -->
-        <script src="/js/jquery.nav.js"></script>
-		<!-- Slicknav JS -->
-		<script src="/js/slicknav.min.js"></script>
-		<!-- ScrollUp JS -->
-        <script src="/js/jquery.scrollUp.min.js"></script>
-		<!-- Niceselect JS -->
-		<script src="/js/niceselect.js"></script>
-		<!-- Tilt Jquery JS -->
-		<script src="/js/tilt.jquery.min.js"></script>
-		<!-- Owl Carousel JS -->
-        <script src="/js/owl-carousel.js"></script>
-		<!-- counterup JS -->
-		<script src="/js/jquery.counterup.min.js"></script>
-		<!-- Steller JS -->
-		<script src="/js/steller.js"></script>
-		<!-- Wow JS -->
-		<script src="/js/wow.min.js"></script>
-		<!-- Magnific Popup JS -->
-		<script src="/js/jquery.magnific-popup.min.js"></script>
-		<!-- Counter Up CDN JS -->
-		<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
-		<!-- Bootstrap JS -->
-		<script src="/js/bootstrap.min.js"></script>
-		<!-- Main JS -->
-		<script src="/js/main.js"></script>
+		<?php
+			include("../templates/importfooter.php");
+			include("../templates/importjs.php");
+		?>
     </body>
 </html>
---
+++ templates/importfooter.php
@@ -0,0 +1,88 @@
+
+		<!-- End Header Area -->
+		<footer id="footer" class="footer ">
+			<!-- Footer Top -->
+			<div class="footer-top">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-3 col-md-6 col-12">
+							<div class="single-footer">
+								<h2>hakkımızda</h2>
+								<p>sosyal medya hesaplarımız</p>
+								<!-- Social -->
+								<ul class="social">
+									<li><a href="/#"><i class="icofont-facebook"></i></a></li>
+									<li><a href="/#"><i class="icofont-google-plus"></i></a></li>
+									<li><a href="/#"><i class="icofont-twitter"></i></a></li>
+									<li><a href="/#"><i class="icofont-vimeo"></i></a></li>
+									<li><a href="/#"><i class="icofont-pinterest"></i></a></li>
+								</ul>
+								<!-- End Social -->
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-6 col-12">
+							<div class="single-footer f-link">
+								<h2>Quick Links</h2>
+								<div class="row">
+									<div class="col-lg-6 col-md-6 col-12">
+										<ul>
+											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>anasayfa</a></li>
+											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>hakkımzda</a></li>
+											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>hizmetler</a></li>
+											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>vakalar</a></li>
+											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>diğer bağlantılar</a></li>	
+										</ul>
+									</div>
+									<div class="col-lg-6 col-md-6 col-12">
+										<ul>
+											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>danışmanlık</a></li>
+											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>finans</a></li>
+											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>Testimonials</a></li>
+											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>SSS</a></li>
+											<li><a href="/#"><i class="fa fa-caret-right" aria-hidden="true"></i>bize ulaşın</a></li>	
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-6 col-12">
+							<div class="single-footer">
+								<h2>açık saatler</h2>
+								<p>ayarlanacak!!!!</p>
+								<ul class="time-sidual">
+									<li class="day">Monday - Fridayp <span>8.00-20.00</span></li>
+									<li class="day">Saturday <span>9.00-18.30</span></li>
+									<li class="day">Monday - Thusday <span>9.00-15.00</span></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-6 col-12">
+							<div class="single-footer">
+								<h2>Newsletter</h2>
+								<p>en yeni haberler için kayıt olmayı unutmayın</p>
+								<form action="mail/mail.php" method="get" target="_blank" class="newsletter-inner">
+									<input name="email" placeholder="Email Address" class="common-input" onfocus="this.placeholder = ''"
+										onblur="this.placeholder = 'Your email address'" required="" type="email">
+									<button class="button"><i class="icofont icofont-paper-plane"></i></button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Footer Top -->
+			<!-- Copyright -->
+			<div class="copyright">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 col-md-12 col-12">
+							<div class="copyright-content">
+								<p>© Copyright 2024  |  All Rights Reserved by <a href="https://www.wpthemesgrid.com" target="_blank">kocaeli üniversitesi</a> </p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Copyright -->
+		</footer>
+		<!--/ End Footer Area -->
